70540b846b542fb7961142be3e8eaaed5d2e06b7
Augusto Neftalí Ruiz Cauich <augusto.ruizcauich@gmail.com>
2022-02-08 11:02:47 -0500

    Add models and migration files to core

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+class PokemonTrainer(models.Model):
+    name = models.CharField('Trainer name', max_length=60)
+    hometown = models.CharField('Hometown', max_length=20)
+
+
+class Pokemon(models.Model):
+    name = models.CharField('Pokemon name', max_length=60)
+    height = models.PositiveIntegerField()
+    weight = models.PositiveBigIntegerField()
+
+
+class Ability(models.Model):
+    name = models.CharField(max_length=60)
+    pokemon = models.ForeignKey(
+        'Pokemon', 
+        related_name='abilities', 
+        on_delete=models.CASCADE
+    )
+
+
+class PokemonTeam(models.Model):
+    owner = models.ForeignKey(
+        'PokemonTrainer', 
+        related_name='teams', 
+        on_delete=models.CASCADE
+    )
+
+    pokemons = models.ManyToManyField('Pokemon', related_name='teams') 
