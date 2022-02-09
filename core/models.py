5c1a99e9d297503d7191aa23d70700877f619269
Augusto Neftalí Ruiz Cauich <augusto.ruizcauich@gmail.com>
2022-02-09 12:24:51 -0500

    Alter Ability.pokemon to allow null and add strs
    The pokemon.abilities.set used in PokemonSerializer class
    does not remove the missing abilities (those that the user
    wants to remove from the relation) if the foreign key does
    not accept null.
    
    __str__ methods are included too.

--- core/models.py
+++ core/models.py
@@ -5,11 +5,17 @@ class PokemonTrainer(models.Model):
     name = models.CharField('Trainer name', max_length=60)
     hometown = models.CharField('Hometown', max_length=20)
 
+    def __str__(self) -> str:
+        return f'{self.name} from {self.hometown}'
+
 
 class Pokemon(models.Model):
     name = models.CharField('Pokemon name', max_length=60)
     height = models.PositiveIntegerField()
     weight = models.PositiveBigIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Ability(models.Model):
@@ -17,8 +23,12 @@ class Ability(models.Model):
     pokemon = models.ForeignKey(
         'Pokemon', 
         related_name='abilities', 
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        null=True
     )
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class PokemonTeam(models.Model):
